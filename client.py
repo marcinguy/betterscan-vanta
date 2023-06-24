34fe577b2d6fb7a61686dcb2a1edbb1218d0fe00
Marcin Kozlowski <marcinguy@gmail.com>
2023-06-24 15:24:44 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,3 @@
-'''
-Need cleanup. WIP
-'''
-
 import os
 import secrets
 import logging
@@ -36,9 +32,6 @@ except:
   tokens["refresh_token"] = ""
 
 
-
-
-
 if not tokens["access_token"]:
   print("here")
   service_information = ServiceInformation('https://app.vanta.com/oauth/authorize',
@@ -60,8 +53,7 @@ if not tokens["access_token"]:
   # (as long you put the host part of your redirect uri in your host file)
   # when the server gets the request with the code (or error) in its query parameters
   _logger.debug('Code got = %s', code)
-  # manager.init_with_authorize_code(redirect_uri, code+"&source_id=app1234")
-  # pprint.pprint(manager)
+
 
   payload = {'client_id': client_id,
            'client_secret': client_secret,
@@ -76,8 +68,7 @@ if not tokens["access_token"]:
   response = json.loads(x.text)
   access_token = response['access_token']
   refresh_token = response['refresh_token']
-  # _logger.debug('Access got = %s', manager._access_token)
-  # Here access and refresh token may be used with self.refresh_token
+
   tokens["access_token"] = access_token
   tokens["refresh_token"] = refresh_token
   pprint.pprint(tokens)
@@ -85,6 +76,8 @@ if not tokens["access_token"]:
   f = open("tokens.json","w")
   f.write(json.dumps(tokens))
   f.close()
+
+
 '''
 os.environ["CODE_DIR"] = os.getcwd()
 os.system("docker run -e CODE_DIR -e LIC -e SNYK_TOKEN -v ${PWD}:${PWD}  -ti  scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'cd $CODE_DIR && git config --global --add safe.directory $CODE_DIR &&  checkmate init'")
@@ -92,6 +85,7 @@ os.system("docker run -e CODE_DIR -e LIC -e SNYK_TOKEN -v ${PWD}:${PWD}  -ti  sc
 os.system("docker run -e CODE_DIR -e LIC -e SNYK_TOKEN -v ${PWD}:${PWD}  -ti  scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'cd $CODE_DIR && git config --global --add safe.directory $CODE_DIR && checkmate git analyze --branch `git rev-parse --abbrev-ref HEAD`'")
 os.system("docker run -e CODE_DIR -e LIC -e SNYK_TOKEN -v ${PWD}:${PWD}  -ti  scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'cd $CODE_DIR && git config --global --add safe.directory $CODE_DIR && checkmate issues html'")
 '''
+
 url = "https://api.vanta.com/v1/resources/vulnerable_component/sync_all"
 
 f = open("report.json","r")
@@ -157,15 +151,11 @@ if response.status_code == 401:
     print(response.text)
 
 
-
-#response = json.loads(response.text)
-
 f = open("tokens.json", "r")
 tokens = json.load(f)
 f.close()
 
 url = "https://api.vanta.com/v1/resources/static_analysis_code_vulnerability_connectors/sync_all"
-
 
 
 payload = {}
@@ -208,8 +198,10 @@ headers = {
     "content-type": "application/json",
     "authorization": "Bearer "+tokens["access_token"]
 }
+
 print(headers)
 print(json.dumps(payload))
+
 response = requests.put(url, json=payload, headers=headers)
 print(response.text)
 
